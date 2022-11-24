50877a35efad9e347300cb89e2c48f8f7c5faec9
Jérémy Lorette <jeremy.lorette@outlook.com>
2022-11-24 01:19:26 +0100

    login form with react hook form and yup

--- frontend/src/components/Login/components/Form/Form.jsx
+++ frontend/src/components/Login/components/Form/Form.jsx
@@ -1,52 +1,89 @@
 import React, { useState } from "react";
 import "./Form.module.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { registerForm } from "../../../../store/login.store.jsx";
-import checkPasswordStrong from "../../../../tools/regexPassword";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import yupPassword from "yup-password";
+yupPassword(yup);
+
+const schema = yup
+  .object({
+    pseudo: yup
+      .string()
+      .min(5, "Le pseudo doit contenir au moins 5 caractères !")
+      .required("Pseudo requis !"),
+
+    email: yup
+      .string()
+      .email("Veuillez entrer un email valide !")
+      .required("Email requis !"),
+    password: yup
+      .string()
+      .min(8, "Le mot de passe doit contenir au moins 8 caractères !")
+      .minLowercase(
+        1,
+        "Le mot de passe doit contenir au moins une lettre minuscule !"
+      )
+      .minUppercase(
+        1,
+        "Le mot de passe doit contenir au moins une lettre majuscule"
+      )
+      .minNumbers(1, "Le mot de passe doit contenir au moins un chiffe")
+      .minSymbols(1, "Le mot de passe doit contenir au moins un symbole")
+      .required("Le mot de passe est requis !"),
+  })
+  .required();
 
 export default function Form() {
-  const formType = useSelector((state) => state.login);
-  const dispatch = useDispatch();
-  const [strongPassword, setStrongPassword] = useState(true);
+  const [registerForm, setRegisterForm] = useState(false);
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
-    <form>
-      {formType && (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {registerForm && (
         <div>
-          <input type="text" placeholder="Pseudo" required />
+          <input type="text" placeholder="Pseudo" {...register("pseudo")} />
+          {errors.pseudo && <span>{errors.pseudo.message}</span>}
         </div>
       )}
       <div>
-        <input type="email" placeholder="email" required />
+        <input type="text" placeholder="email" {...register("email")} />
+        {errors.email && <span>{errors.email.message}</span>}
       </div>
+      {registerForm ? (
+        <div>
+          <input
+            type="password"
+            placeholder="mots de passe"
+            {...register("password")}
+          />
+          {errors.password && <span>{errors.password.message}</span>}
+        </div>
+      ) : (
+        <div>
+          <input type="password" placeholder="mots de passe" />
+        </div>
+      )}
+
       <div>
-        <input
-          onChange={(e) => {
-            formType && checkPasswordStrong(e.target.value)
-              ? setStrongPassword(true)
-              : setStrongPassword(false);
-          }}
-          type="password"
-          placeholder="mots de passe"
-          required
-        />
-        {!strongPassword && formType && (
-          <span>
-            Le mot de passe doit contenir au moins 1 caractère spécial, 1
-            majuscule, 1 minuscule, 1 chiffre et 8 caractères !
-          </span>
-        )}
-      </div>
-      <div>
-        {formType ? (
+        {registerForm ? (
           <button type="submit">S'inscrire</button>
         ) : (
           <button type="submit">Connexion</button>
         )}
-        {formType ? (
+        {registerForm ? (
           <a
             onClick={(e) => {
-              dispatch(registerForm(formType));
+              setRegisterForm(false);
             }}
           >
             Se connecter
@@ -54,7 +91,7 @@ export default function Form() {
         ) : (
           <a
             onClick={(e) => {
-              dispatch(registerForm(formType));
+              setRegisterForm(true);
             }}
           >
             Crée un compte
--- frontend/src/tools/regexPassword.js
+++
@@ -1,9 +0,0 @@
-const strongPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
-
-export default function checkPasswordStrong(value) {
-  if (value.match(strongPassword)) {
-    return true;
-  } else {
-    return false;
-  }
-}
