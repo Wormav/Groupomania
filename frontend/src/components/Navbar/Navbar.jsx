7688b71af4ec656454f84081148ba9e4d5e22d61
Jérémy Lorette <jeremy.lorette@outlook.com>
2022-11-29 13:24:41 +0100

    base navBar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,44 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import styles from "./Navbar.module.scss";
 
 export default function Navbar() {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [widthScreen, setWidthScreen] = useState(window.innerWidth);
+
+  const toggleNavSmallScreen = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setWidthScreen(window.innerWidth);
+    };
+
+    if (window.innerWidth > 500) {
+      setToggleMenu(false);
+    }
+
+    window.addEventListener("resize", changeWidth);
+
+    return () => {
+      window.addEventListener("resize", changeWidth);
+    };
+  }, []);
+
   return (
     <nav className={`${styles.navBar}`}>
-      <ul className={`${styles.liste}`}>
-        <li className={`${styles.items}`}>Acceuil</li>
-        <li className={`${styles.items}`}>Profil</li>
-        <li className={`${styles.items}`}>Message</li>
-      </ul>
-      <button className={`${styles.btn}`}>btn</button>
+      {(toggleMenu || widthScreen > 500) && (
+        <ul className={`${styles.liste}`}>
+          <li className={`${styles.items}`}>Acceuil</li>
+          <li className={`${styles.items}`}>Profil</li>
+          <li className={`${styles.items}`}>Message</li>
+        </ul>
+      )}
+      <button onClick={toggleNavSmallScreen} className={`${styles.btn}`}>
+        btn
+      </button>
     </nav>
   );
 }
--- frontend/src/components/Routes/Routes.jsx
+++ frontend/src/components/Routes/Routes.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import "./Routes.module.scss";
+import styles from "./Routes.module.scss";
 import Navbar from "../Navbar/Navbar";
 
 export default function RoutesApp() {
   return (
-    <>
+    <div className={`${styles.divRouteContainer}`}>
       <Navbar />
       <Routes>
         <Route path="/" element={<div>route</div>} />
         <Route path="*" element={<div>404</div>} />
       </Routes>
-    </>
+    </div>
   );
 }
