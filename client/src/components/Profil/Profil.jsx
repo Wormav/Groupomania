bdcb2b4781486f4645d5f45d453525b4534ad673
Jérémy Lorette <jeremy.lorette@outlook.com>
2022-12-01 14:34:29 +0100

    base style page profil

--- client/src/components/Profil/Profil.jsx
+++ client/src/components/Profil/Profil.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import styles from "./Profil.module.scss";
+import { FiMail } from "react-icons/fi";
 
 export default function Profil() {
   const dataUser = useSelector((state) => state.user);
@@ -7,11 +9,35 @@ export default function Profil() {
   return (
     <>
       {dataUser && (
-        <>
-          <h1>{dataUser.user_username}</h1>
-          <img src={dataUser.user_picture}></img>
-          <p>{dataUser.user_email}</p>
-        </>
+        <div className={`${styles.container}`}>
+          <img
+            className={`${styles.picture}`}
+            src={dataUser.user_picture}
+          ></img>
+          <div className={`${styles.card}`}>
+            <h1 className={`${styles.pseudo}`}>{dataUser.user_username}</h1>
+            <div className={`${styles.email_container}`}>
+              <FiMail className={`${styles.email_icon}`} />
+              <a
+                href={`mailto: ` + `${dataUser.user_email}`}
+                className={`${styles.email}`}
+              >
+                {dataUser.user_email}
+              </a>
+            </div>
+            <div className={`${styles.bio_container}`}>
+              <div className={`${styles.bio_title_container}`}>
+                <h2 className={`${styles.bio_title}`}>À propos</h2>
+                <div className={`${styles.bio_title_element}`}></div>
+              </div>
+              <p className={`${styles.bio}`}>{dataUser.user_bio}</p>
+            </div>
+            <div className={`${styles.btn_container}`}>
+              <button className={`${styles.btn}`}>Suivre</button>
+              <button className={`${styles.btn_reverse}`}>Message</button>
+            </div>
+          </div>
+        </div>
       )}
     </>
   );
